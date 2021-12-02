332b8520de19697b3b4fb9118b37d78119548014
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-02 11:34:45 -0500

    add background to landing page and stub up general idea

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,26 @@
 import styles from './Landing.module.css'
+import { Link } from 'react-router-dom'
 
-const Landing = ({user}) => {
+
+const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>
-        hello, {user ? user.name : "friend"}
-      </h1>
-    </main>
+    <>
+      <main className={styles.container}>
+        <div className="bg-light-blue">
+          <div className="box-border">
+            <h1>
+              Welcome{user ? `, ${user.name} Click Explore to Get Started!!` : "."}
+            </h1>
+          </div>
+          <div>
+            <p>About Us:</p>
+          </div>
+            <Link className="btn" to="/login">Login</Link>
+            <Link className="btn" to="/signup">Sign Up</Link>
+        </div>
+      </main>
+    </>
   )
 }
- 
+
 export default Landing
