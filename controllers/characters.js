97141a3f3b203b1aed9d37d884818d95da199e76
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-06 15:25:08 -0500

    push character details into profile character array and refactor profile controller

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,56 +1,70 @@
 import axios from 'axios'
 import { Character } from '../models/character.js'
+import { Profile } from '../models/profile.js'
 const BASE_URL = 'https://api.genshin.dev'
 
-function index(req, res){
+function index(req, res) {
    axios.get(`${BASE_URL}/characters`)
-   .then(characters => {
-      res.json(characters.data)
-   })
-   .catch(function (error) {
-      console.log(error);
-   })
-}
-
-function show(req, res){
-   axios.get(`${BASE_URL}/characters/${req.params.character}`)
-   .then (characterInfo =>{
-      res.json(characterInfo.data)
-   })
-   .catch(function (error) {
-      console.log(error);
-   })
-}
-
-function create(req,res){
-   Character.findOne({characterName: req.body.characterName})
-   .then (character => {
-      if (character) { 
-         character.collectedBy.push(req.user.profile)
-         character.save()
-      } else { 
-         Character.create(req.body)
-         .then(character => {
-            character.collectedBy = req.user.profile
-            character.save()
-            res.json(character)
-         })
-         .catch(function (error) {
-            console.log(error);
-         })
-      }
-   })
-}
-
-function removeFromCollection(req,res) {
-   Character.findOne({characterName: req.params.character})
-   .then (character => {
-      let index = character.collectedBy.findIndex(function(element){
-         return element.toString() === req.user.profile;
+      .then(characters => {
+         res.json(characters.data)
       })
-      character.collectedBy.splice(index,1)
-      character.save()
-   })
+      .catch(function (error) {
+         console.log(error);
+      })
+}
+
+function show(req, res) {
+   axios.get(`${BASE_URL}/characters/${req.params.character}`)
+      .then(characterInfo => {
+         res.json(characterInfo.data)
+      })
+      .catch(function (error) {
+         console.log(error);
+      })
+}
+
+function create(req, res) {
+   Character.findOne({ characterName: req.body.characterName })
+      .then(character => {
+         //If character exists
+         if (character) {
+            character.collectedBy.push(req.user)
+            character.save()
+            Profile.findById(req.user.profile)
+               .then(profile => {
+                  profile.characters.push(character._id)
+                  profile.save()
+                  //end here and respond
+                  res.json(character)
+               })
+         } else {
+            Character.create(req.body)
+               .then(character => {
+                  character.collectedBy = req.user.profile
+                  character.save()
+                  Profile.findById(req.user.profile)
+                     .then(profile => {
+                        profile.characters.push(character._id)
+                        profile.save()
+                        res.json(character)
+                     })
+               }
+               ).catch(err => {
+                  console.log(err)
+               })
+         }
+      })
+}
+
+function removeFromCollection(req, res) {
+   Character.findOne({ characterName: req.params.character })
+      .then(character => {
+         let index = character.collectedBy.findIndex(function (element) {
+            return element.toString() === req.user.profile;
+         })
+         character.collectedBy.splice(index, 1)
+         character.save()
+      })
 }
 
 export {
