dcfe677cea00f519ce3efecab18b7e389b961d96
UbuntuLex <alexandermaniwang99@gmail.com>
2021-12-08 15:33:54 -0500

    use bulma for navbar refactor

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,43 +4,54 @@ import { Link } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
 	return (
 		<>
-			<nav className="navbar navbar-light bg-light">
-				<div>
-					<Link to="/">
+			<nav className="navbar">
+				<div className="navbar-brand">
+					<Link to="/" className="navbar-item">
 						{/* Temporary Image */}
-						<img src="https://ih1.redbubble.net/image.1816083712.5142/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" alt="placeholder" width="30" height="30" className="d-inline-block align-text-top" />
+						<img src="https://ih1.redbubble.net/image.1816083712.5142/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+							alt="placeholder"
+							width="30"
+							height="30" />
 						Genshin Builds
 					</Link>
+
+					<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="genshin-navbar">
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+						<span aria-hidden="true"></span>
+					</a>
 				</div>
-				<ul className="nav nav-pills">
-					<li className="nav-item">
-						<Link className="nav-link" aria-current="page" to="/">Home</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" aria-current="page" to="/profile">My Profile</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" aria-current="page" to="/characters">Characters</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" aria-current="page" to="/weapons">Weapons</Link>
-					</li>
-					{!user &&
-						<>
-							<li className="nav-item">
-								<Link className="nav-link" aria-current="page" to="/login">Login</Link>
-							</li>
-							<li className="nav-item">
-								<Link className="nav-link" aria-current="page" to="/signup">Sign Up</Link>
-							</li>
-						</>
-					}
-					{user &&
-						<li className="nav-item">
-							<Link to='' className="nav-link" onClick={handleLogout}>Log Out</Link>
-						</li>
-					}
-				</ul>
+
+				<div className="navbar-menu" id="genshin-navbar">
+					<div className="navbar-start">
+						<Link className="navbar-item" to="/">Home</Link>
+						<Link className="navbar-item" to="/profile">My Profile</Link>
+						<Link className="navbar-item" to="/characters">Characters</Link>
+						<Link className="navbar-item" to="/weapons">Weapons</Link>
+					</div>
+
+					<div className="navbar-end">
+						{!user &&
+							<>
+								<div className="navbar-item">
+									<div className="buttons">
+										<Link className="button is-light" to="/login">Login</Link>
+										<Link className="button is-info" to="/signup">Sign Up</Link>
+									</div>
+								</div>
+							</>
+						}
+						{user &&
+							<>
+								<div className="navbar-item">
+									<div className="buttons">
+										<Link to='' className="button is-light" onClick={handleLogout}>Log Out</Link>
+									</div>
+								</div>
+							</>
+						}
+					</div>
+				</div>
 			</nav>
 		</>
 	)
