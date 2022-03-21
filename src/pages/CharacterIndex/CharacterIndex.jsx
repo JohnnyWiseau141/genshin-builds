c126659488ec4002b50341bf37910417fde8905b
UbuntuLex <alexandermaniwang99@gmail.com>
2022-03-21 16:48:54 -0400

    Refactor background css and character containtercss

--- src/pages/CharacterIndex/CharacterIndex.jsx
+++ src/pages/CharacterIndex/CharacterIndex.jsx
@@ -2,22 +2,22 @@ import styles from './CharacterIndex.module.css'
 import { Link } from 'react-router-dom'
 
 const CharacterIndex = (props) => {
-  return (  
+  return (
     <>
-  <div className={styles.bground}>
-    <div className={styles.container}>
-      {props.characters.map(character =>
-        <div key={character} className={styles.icon}>
-              <Link to={`/characterDetails`} state={character}> 
+      <div className={styles.bground}>
+        <div className={styles.container}>
+          {props.characters.map(character =>
+            <div key={character} className={styles.icon}>
+              <Link to={`/characterDetails`} state={character}>
                 <img className={styles.characterIcon} src={`https://api.genshin.dev/characters/${character}/icon`} alt="icon" />
-                <div className={styles.box}>{character}</div>
+                <div className={styles.box}>{character[0].toUpperCase() + character.slice(1,20)}</div>
               </Link>
+            </div>
+          )}
         </div>
-      )}
-    </div>
-    </div>
+      </div>
     </>
   );
 }
- 
+
 export default CharacterIndex;
