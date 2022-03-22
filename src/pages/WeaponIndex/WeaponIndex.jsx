52c90e5e513a75ce63a618b9d5d5c30b19a2303d
UbuntuLex <alexandermaniwang99@gmail.com>
2022-03-22 13:30:38 -0400

    restyle weapon index matching character index page

--- src/pages/WeaponIndex/WeaponIndex.jsx
+++ src/pages/WeaponIndex/WeaponIndex.jsx
@@ -2,20 +2,25 @@ import styles from './WeaponIndex.module.css'
 import { Link } from 'react-router-dom'
 
 const WeaponIndex = (props) => {
-  return (  
+  return (
     <>
-    <div className={styles.container}>
-      {props.weapons.map(weapon =>
-        <div key={weapon} className={styles.weaponImage}>
-              <Link to={`/weaponDetails`} state={weapon}> 
+      <div className={styles.bground}>
+        <div className={styles.container}>
+          {props.weapons.map(weapon =>
+            <div key={weapon} className={styles.weaponImage}>
+              <Link to={`/weaponDetails`} state={weapon}>
                 <img className={styles.weaponIcon} src={`https://api.genshin.dev/weapons/${weapon}/icon`} alt="Weapon" />
-                <div className={styles.box}>{weapon}</div>
+                {/* add conditional that fixes broken images */}
+
+                <div className={styles.box}>{weapon[0].toUpperCase() + weapon.slice(1, 20)}</div>
+                {/* add conditional that removes '-', replaces it with spaces, and capitilize second+ word(s) */}
               </Link>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
     </>
   );
 }
- 
+
 export default WeaponIndex;
