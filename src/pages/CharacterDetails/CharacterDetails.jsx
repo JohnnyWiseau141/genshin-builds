2ca95ab81818884f11ce238f8b2f55322c0f830a
UbuntuLex <alexandermaniwang99@gmail.com>
2022-03-22 13:50:17 -0400

    navbar no longer pushes content down when using smaller screen sizes

--- src/pages/CharacterDetails/CharacterDetails.jsx
+++ src/pages/CharacterDetails/CharacterDetails.jsx
@@ -18,26 +18,28 @@ const CharacterDetails = (props) => {
   return (
     <>
       {/* render character gacha card */}
-      <div className={styles.detailsContainer} >
-        <div className={styles.cardContainer}>
-          <img className={styles.gachaCard} src={`https://api.genshin.dev/characters/${location.state}/gacha-card`} alt="gacha-card" />
-        </div>
-
-        <div className={styles.btnTxt}>
-          {/* render character details */}
-          <div className={styles.characterDetails}>
-            <h1>Name: {characterDetails.name}</h1>
-            <h1>Vision: {characterDetails.vision}</h1>
-            <h1>Weapon: {characterDetails.weapon}</h1>
-            <h1>Nation: {characterDetails.nation}</h1>
-            <h1>Rarity: {characterDetails.rarity}</h1>
-          </div>
-          <div>
-            <CharacterForm detailId={location.state} characterDetails={characterDetails} />
+      <div className={styles.bground}>
+        <div className={styles.detailsContainer} >
+          <div className={styles.cardContainer}>
+            <img className={styles.gachaCard} src={`https://api.genshin.dev/characters/${location.state}/gacha-card`} alt="gacha-card" />
           </div>
 
-          <div>
-            <RemoveCharacter detailId={location.state} characterDetails={characterDetails} />
+          <div className={styles.btnTxt}>
+            {/* render character details */}
+            <div className={styles.characterDetails}>
+              <h1>Name: {characterDetails.name}</h1>
+              <h1>Vision: {characterDetails.vision}</h1>
+              <h1>Weapon: {characterDetails.weapon}</h1>
+              <h1>Nation: {characterDetails.nation}</h1>
+              <h1>Rarity: {characterDetails.rarity}</h1>
+            </div>
+            <div>
+              <CharacterForm detailId={location.state} characterDetails={characterDetails} />
+            </div>
+
+            <div>
+              <RemoveCharacter detailId={location.state} characterDetails={characterDetails} />
+            </div>
           </div>
         </div>
       </div>
