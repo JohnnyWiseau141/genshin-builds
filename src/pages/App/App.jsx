665ff575e7ab76aab5d339bded6d563c2f177b59
UbuntuLex <alexandermaniwang99@gmail.com>
2022-04-19 18:46:32 -0400

    stub add news page

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -12,6 +12,7 @@ import CharacterDetails from '../CharacterDetails/CharacterDetails'
 import WeaponDetails from '../WeaponDetails/WeaponDetails'
 import CreateTeamBuild from '../CreateTeamBuild/CreateTeamBuild'
 import Community from '../Community/Community'
+import AddNews from '../../components/AddNews/AddNews'
 import * as authService from '../../services/authService'
 import { getAllCharacters } from '../../services/characterService'
 import { getAllWeapons } from '../../services/weaponService'
@@ -72,6 +73,7 @@ const App = () => {
 				<Route path='/weapon-details/:weapon' element={<WeaponDetails user={user} weapons={weapons} />} />
 				<Route path='/create-team-build' element={<CreateTeamBuild user={user} weapons={weapons} characters={characters} />} />
 				<Route path='/community' element={<Community user={user} weapons={weapons} characters={characters} />} />
+				<Route path='/add-news' element={<AddNews user={user} />} />
 			</Routes>
 		</>
 	);
